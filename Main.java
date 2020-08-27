e391df3efc2f6f98b5ba2a62fb8d3b957d139d10
Ivan6274 <Isivanov2011@gmail.com>
2020-08-27 23:00:49 +0300

    CreditPaymentService app fixed

--- Main.java
+++ Main.java
@@ -2,7 +2,14 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-         double monthlyPayment = service.calculate(1000000 , 9.99 ,36 );
+
+        double monthlyPayment = service.calculate(1000000, 9.99, 12);
         System.out.println("Ваш ежемесячный платеж равен = " + monthlyPayment);
+
+        double monthlyPayment1 = service.calculate(1000000, 9.99, 24);
+        System.out.println("Ваш ежемесячный платеж равен = " + monthlyPayment1);
+
+        double monthlyPayment2 = service.calculate(1000000, 9.99, 36);
+        System.out.println("Ваш ежемесячный платеж равен = " + monthlyPayment2);
     }
 }
